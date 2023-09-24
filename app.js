a701765634e76fc51d89eeabd2603af16bc53da2
Diego Hernandez <diegofootball19@gmail.com>
2023-09-24 17:46:20 -0500

    add components files

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import Store from "./services/Store.js"
 import API from "./services/API.js"
+import Router from "./services/Router.js"
 import { loadData } from "./services/Menu.js"
+import { MenuPage } from "./components/MenuPage.js"
+import { DetailsPage } from "./components/DetailsPage.js"
+import { OrderPage } from "./components/OrderPage.js"
+
 window.app = {}
 app.store = Store;
-
+app.router = Router;
 
 // It's better to wait for the event  for manipulation 
 window.addEventListener("DOMContentLoaded", () => {
     loadData();
+    app.router.init();
 });
